cbe6c1d2cefc37982a0b150b6584e91cbd31b5b5
Mandeep Kour <67736304+mandeep617@users.noreply.github.com>
2023-07-20 20:47:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [players, setPlayers] = useState([]);
+  const [filteredPlayers, setFilteredPlayers] = useState([]);
   const [isDataFetched, setIsDataFetched] = useState(false);
 
-  const handleFetchClick = () => {
+  const positions = ['all','C', 'LW', 'D'];
+  const fetchAllPlayers = (position) => {
     fetch('http://localhost:4000/fetch-player')
       .then(response => {
         if (!response.ok) {
@@ -14,36 +15,48 @@ function App() {
         return response.json();
       })
       .then(data => {
-        setPlayers(data);
         setIsDataFetched(true);
+        if (position === 'all') {
+          setFilteredPlayers(data);
+        } else {
+          const filtered = data.filter(player => player.position === position);
+          setFilteredPlayers(filtered);
+        }
       })
       .catch(error => {
         console.error('Error fetching data:', error);
       });
+
+   
   };
+
 
   return (
     <div className="container">
-      <h1>Player List</h1>
-      <button onClick={handleFetchClick}>Click to Fetch</button>
+      <h1>Players List</h1>
+
+      {positions.map((position, index) => (
+        <button key={index} onClick={() =>{fetchAllPlayers(position)}}>
+          Show Players with Position '{position}'
+        </button>
+      ))}
       {isDataFetched && (
-        <table>
-          <thead>
-            <tr>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Position</th>
-            </tr>
-          </thead>
-          <tbody>
-            {players.map((player,index) => (
-              <tr key={index}>
-                <td>{player.firstName}</td>
-                <td>{player.lastName}</td>
-                <td>{player.position}</td>
-              </tr>
-            ))}
-          </tbody>
+      <table><thead>
+          <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Position</th>
+        </tr>
+        </thead>
+        <tbody>
+        {filteredPlayers.map((player,index) => (
+          <tr key={index}>
+            <td>{player.firstName}</td>
+            <td>{player.lastName}</td>
+            <td>{player.position}</td>
+          </tr>
+        ))}
+        </tbody>
         </table>
       )}
     </div>
